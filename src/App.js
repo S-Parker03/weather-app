21b622a9cadf5b9963e5cfb452dd7c89934f330d
S-Parker03 <stanleyrp03@gmail.com>
2024-03-11 13:51:20 +0000

    Added divider image, background still isn't changing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css"
 import"./Mobile.css"
 import { useState, useEffect } from 'react';
 const App = () => {
-  const [background, setBackground] = useState("day");
+  var [background, setBackground] = useState();
 
   function changeBackground(){
     console.log("function ran")
@@ -14,35 +14,37 @@ const App = () => {
 
     time = today.getHours()
     console.log(time);
-    switch(time){
-      case(time < 8 && time > 5):
-        setBackground("morning");
-        console.log("morning");
-        break;
-      case(time > 7 && time < 17):
-        setBackground("day");
-        console.log("day");
-        break;
-      case(time > 16 && time < 21):
-        setBackground("evening");
-        console.log("evening");
-        break;
-      case(time > 20 || time < 6):
-        setBackground("night");
-        console.log("night");
-        break;
+    if(time < 8 && time > 5){
+      setBackground("morning");
+      console.log("should be: morning");
+      console.log(background);
+        
+    }else if(time > 7 && time < 17){
+      setBackground("day");
+      console.log("should be: day");
+      console.log(background);
+       
+    }else if(time > 16 && time < 21){
+      setBackground("evening");
+      console.log("should be: evening");
+      console.log(background);
+    
+    }else if(time > 20 || time < 6){
+      setBackground("night");
+      console.log("should be: night");
+      console.log(background);
     }
+
     
   }
 
   useEffect(() => {
     changeBackground();
-    console.log(background);
   }, []);
   
 
   return (
-    <div className ={background}>
+    <div className ="day">
       <LocationBar/>
       <Weather />
       <Waves />
